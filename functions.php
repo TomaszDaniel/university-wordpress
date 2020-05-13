5ef3dcb92a76a1507cb9c768780f1b202cf8dc58
Tomasz Daniel <tomaszz.daniel@gmail.com>
2020-05-13 22:00:21 +0200

    add campus post type and google map API

--- functions.php
+++ functions.php
@@ -33,6 +33,7 @@ function page_banner($args = NULL){
 }
 
 function universisty_files(){
+    wp_enqueue_script('googleMap','//maps.googleapis.com/maps/api/js?key=AIzaSyCuuvYhopln2GJAEj4m07ADw-D9Rq187ds',NULL,microtime(),true);
     wp_enqueue_script('main-university-js',get_theme_file_uri('/js/scripts-bundled.js'),NULL,microtime(),true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awsome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -52,6 +53,10 @@ function university_features(){
 add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query){
+    if(!is_Admin() AND is_post_type_archive('campus') AND $query->is_main_query()){
+        $query -> set('posts_per_page',-1);
+    }
+    
     if(!is_Admin() AND is_post_type_archive('program') AND $query->is_main_query()){
         $query -> set('orderby','title');
         $query -> set('order','ASC');
@@ -75,3 +80,9 @@ function university_adjust_queries($query){
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api){
+    $api['key']= 'AIzaSyCuuvYhopln2GJAEj4m07ADw-D9Rq187ds';
+    return $api;
+}
+add_filter('acf/fields/google_map/api','universityMapKey');
